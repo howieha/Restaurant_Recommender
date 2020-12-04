7d79b569c9cc936f6e9029dae23d6faa027c2eb9
Amelia Pond <ameliapond@Amelias-MacBook-Pro.local>
2020-12-03 23:03:02 -0500

    Collaborative filtering ver.2

--- Collaborative-filtering/user_avg_ratings.py
+++ Collaborative-filtering/user_avg_ratings.py
@@ -32,27 +32,3 @@ if __name__ == "__main__":
         
     user_avg_ratings = get_user_avg_ratings("user_user_similarity/user_item_matrix.tsv")
     user_avg_ratings.to_csv(os.path.join(os.path.dirname(__file__), "user_avg_ratings.tsv"), sep="\t")
-                
-"""
-for line in pd.read_csv(
-        os.path.join(os.path.dirname(__file__), "user_user_similarity/user_item_matrix.tsv"),
-        sep='\t', na_values='None', index_col=0, chunksize=1):
-    user_id = line.index
-    recommendation = set()
-    for bus_id in line:
-        if line[bus_id].item()!=0.0:
-            content_filtered_bus = relev_businesses[bus_id]
-            sorted_relev_bus = sorted(content_filtered_bus, key=content_filtered_bus.get)
-            if not filter_all:
-                if include_been_to:
-                    sorted_relev_bus = sorted_relev_bus[:top_k_bus]
-                else:
-                    sorted_relev_bus = sorted_relev_bus[1:top_k_bus+1]
-            recommendation.update(sorted_relev_bus)
-            content_recommendations[user_id.item()] = recommendation
-    print(line_counter)
-    line_counter += 1
-
-with open(os.path.join(os.path.dirname(__file__), 'content_recommendation.pickle'), 'wb') as handle:
-    pickle.dump(content_recommendations, handle, protocol=pickle.HIGHEST_PROTOCOL)
-"""
